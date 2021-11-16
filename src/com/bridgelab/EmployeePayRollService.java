2d5a0df994766692ed6b04dd1b4f341d5b6fefc0
pitu <ashishkumarvish.432@gmail.com>
2021-11-16 19:06:23 +0530

    To creat employee pay roll service and read and writ to console

---
+++ src/com/bridgelab/EmployeePayRollService.java
@@ -0,0 +1,8 @@
+package com.bridgelab;
+
+public class EmployeePayRollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Service");
+        
+    }
+}
